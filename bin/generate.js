742138c09a19ac7933ced9d5e52c2640b5ddb41f
Kyle E. Mitchell <kyle@kemitchell.com>
2016-02-06 20:15:24 -0800

    Simplify to get build working again

--- bin/generate.js
+++ bin/generate.js
@@ -1,205 +1,108 @@
-var async = require('async');
-var cheerio = require('cheerio'); // HTML parsing
-var fs = require('fs');
-var https = require('https');
-var mustache = require('mustache'); // templating
-var normalize = require('normalize-package-data');
-var packageJSON = require('package-json');
-var parseGitHub = require('parse-github-url');
-var path = require('path');
-var spdx = require('spdx'); // license string validation
-var githubPackageJSON = require('github-package-json');
+var async = require('async')
+var fs = require('fs')
+var mustache = require('mustache')
+var normalize = require('normalize-package-data')
+var npm1k = require('npm1k')
+var packageJSON = require('package-json')
+var parseGitHub = require('parse-github-url')
+var path = require('path')
+var validateLicense = require('validate-npm-package-license')
 
-var API_DELAY = process.env.API_DELAY || 750;
+var categories = ['valid', 'invalid', 'missing']
+var template = fs.readFileSync(
+  path.join(__dirname, '..', 'templates', 'index.html'))
+  .toString()
 
-var offsets = [];
-for (var i = 0; i < 1000; i += 36) {
-  offsets.push(i);
-}
-
-var number = 0;
-
-function packageNames(html) {
-  var $ = cheerio.load(html);
-  return $('a.name')
-    .map(function() {
-      return cheerio(this).text();
-    })
-    .get();
-}
-
-function getFixItURL(repository) {
-  var repoURL = repository.url;
-  if (repoURL && repoURL.indexOf('github.com') > -1) {
-    // github-url-to-object fails with git+<protocol>:// URLs
-    var parsed = parseGitHub(repoURL);
-    if (parsed) {
-      // TODO: use the default branch since master doesn't always
-      // exist
-      return (
-        'https://github.com' +
-        '/' + parsed.user +
-        '/' + parsed.repo +
-        '/edit' +
-        '/master' +
-        '/package.json'
-      );
-    }
-  }
-}
-
-function isValid(json) {
-  return json.license &&
-    typeof json.license === 'string' &&
-    spdx.valid(json.license) === true;
-}
-
-function addRepoFixes(repository, result, callback) {
-  setTimeout(function() {
-    githubPackageJSON.master(
-      repository.url,
-      function(err, repositoryJSON) {
-        if (!err && repositoryJSON && isValid(repositoryJSON)) {
-          result.fixedInRepo = true;
-
-          return callback(null, result);
-        }
-
-        setTimeout(function() {
-          githubPackageJSON.pullRequests(
-            repository.url,
-            function(err, packageJSONs) {
-              if (!err) {
-                var validPRs = packageJSONs
-                  .filter(function(pullRequest) {
-                    return isValid(pullRequest.json);
-                  });
-
-                if (validPRs.length > 0) {
-                  result.fixedInPRs = validPRs
-                    .map(function(pullRequest) {
-                      delete pullRequest.json;
-                      return pullRequest;
-                    });
-
-                  return callback(null, result);
-                }
-              }
-
-              result.fixItURL = getFixItURL(repository);
-
-              callback(null, result);
-            }
-          );
-        }, API_DELAY);
-      }
-    );
-  }, API_DELAY);
-}
-
-function processPackages(packages, callback) {
-  async.mapSeries(
-    packages,
-    function(name, cbMap) {
-      var packageNumber = ++number;
-      packageJSON(name, 'latest', function(error, json) {
-        normalize(json);
-        if (error) {
-          cbMap(error);
-        } else {
-          var missing = !json.license;
-          var valid = (
-            !missing && isValid(json)
-          );
-          var invalid = !valid && !missing;
-          var hasLicenses = json.licenses;
-          var result = {
-            number: packageNumber,
-            package: name,
-            version: json.version,
-            maintainers: json.maintainers,
-            homepage: encodeURI(json.homepage),
-            license: (
-              json.license ?
-                JSON.stringify(json.license) :
-                hasLicenses ?
-                  'Using deprecated "licenses"' :
-                  'None'
-            ),
-            valid: valid,
-            invalid: invalid,
-            missing: missing,
-            dependencies: json.dependencies,
-            devDependencies: json.devDependencies
-          };
-          if ((invalid || missing) && json.repository) {
-            return addRepoFixes(json.repository, result, cbMap);
-          }
-          cbMap(null, result);
-        }
-      });
-    },
-    callback
-  );
-}
-
-function getMostDependedPage(offset, callback) {
-  https.get({
-    hostname: 'www.npmjs.com',
-    path: '/browse/depended?offset=' + offset
-  }, function(response) {
-    var buffer = '';
-    response
-      .on('data', function(data) {
-        buffer += data.toString();
-      })
-      .on('end', function() {
-        callback(buffer);
-      });
-  });
-}
-
-async.concatSeries(
-  offsets,
-  function(offset, callback) {
-    getMostDependedPage(offset, function(html) {
-      processPackages(packageNames(html), callback);
-    });
-  },
-  function(error, packages) {
-    if (error) {
-      throw error;
-    } else {
-      packages = packages
-        .slice(0, 1000);
+npm1k(function (error, packages) {
+  if (error) throw error
+  else {
+    processPackages(packages, function (error, packages) {
+      if (error) throw error
       var mostWanted = packages
-        .filter(function(package) {
-          return !package.valid;
-        });
+        .filter(function (p) {
+          return !p.licenseData.validForNewPackages
+        })
+      function percent (argument) {
+        return argument / packages.length * 100
+      }
+      var valid = percent(packages.reduce(function (count, p) {
+        return count + (p.licenseData.validForNewPackages ? 1 : 0)
+      }, 0))
       var context = {
         date: new Date().toUTCString(),
-        valid: packages.reduce(function(count, element) {
-          return count + (element.valid ? 1 : 0);
-        }, 0) / packages.length * 100,
-        invalid: packages.reduce(function(count, element) {
-          return count + (element.invalid ? 1 : 0);
-        }, 0) / packages.length * 100,
-        missing: packages.reduce(function(count, element) {
-          return count + (element.missing ? 1 : 0);
-        }, 0) / packages.length * 100,
+        valid: valid,
+        invalid: 100 - valid,
         mostWanted: mostWanted
-      };
-      ['valid', 'invalid', 'missing'].forEach(function(count) {
-        context[count + 'Rounded'] = Math.round(context[count]);
-      });
-      console.log(
-        mustache.render(
-          fs.readFileSync(
-            path.join(__dirname, '..', 'templates', 'index.html')
-          ).toString(),
-          context
-        )
-      );
+      }
+      categories.forEach(function (count) {
+        context[count + 'Rounded'] = Math.round(context[count])
+      })
+      process.stdout.write(mustache.render(template, context))
+    })
+  }
+})
+
+function processPackages (packages, callback) {
+  var number = 0
+  async.mapSeries(
+    packages,
+    function (name, next) {
+      var packageNumber = ++number
+      packageJSON(name, 'latest', function (error, json) {
+        var result = { number: packageNumber, package: name }
+        if (error) {
+          result.error = 'Could not fetch package.json'
+          result.licenseData = {
+            validForNewPackages: false,
+            warnings: [ 'Could not fetch package.json' ]
+          }
+          next(null, result)
+        } else {
+          normalize(json)
+          result.number = packageNumber
+          result.package = name
+          result.version = json.version
+          result.maintainers = json.maintainers
+          result.homepage = json.homepage
+            ? encodeURI(json.homepage)
+            : 'https://www.npmjs.com/packages/' + name
+          if (json.license) {
+            if (typeof json.license === 'string') {
+              result.licenseData = validateLicense(json.license)
+            } else {
+              result.licenseData = {
+                validForNewPackages: false,
+                warnings: [ 'Invalid license property' ]
+              }
+            }
+          } else {
+            result.licenseData = {
+              validForNewPackages: false,
+              warnings: [ 'Missing license property' ]
+            }
+          }
+          result.license = JSON.stringify(json.license)
+          if (json.repository) {
+            result.fixItURL = getFixItURL(json.repository)
+          }
+          next(null, result)
+        }
+      })
+    },
+    callback
+  )
+}
+
+function getFixItURL (repository) {
+  var repoURL = repository.url
+  if (repoURL && repoURL.indexOf('github.com') > -1) {
+    var parsed = parseGitHub(
+      repoURL.replace('git+https://', 'https://'))
+    if (parsed) {
+      return (
+        'https://github.com' + '/' + parsed.user + '/' + parsed.repo +
+        '/edit' + '/master' + '/package.json'
+      )
     }
   }
-);
+}
